87df5afbfca66daae1b142c515aa7ccda15ddd47
Graham Otte <graham.otte@appfolio.com>
2016-12-20 18:22:09 -0800

    tabs

--- lib/client.rb
+++ lib/client.rb
@@ -1,13 +1,13 @@
 class Client < XMLRPC::Client
-	def fetch(queries)
-		call_async('d.multicall', 'main', *render_queries(queries)).map do |t|
-			Torrent.new(t, queries)
-		end
-	end
+  def fetch(queries)
+    call_async('d.multicall', 'main', *render_queries(queries)).map do |t|
+      Torrent.new(t, queries)
+    end
+  end
 
-	private
+  private
 
-	def render_queries(queries)
+  def render_queries(queries)
     queries.map { |k, v| "d.#{v[:call]}=" }
   end
 end
